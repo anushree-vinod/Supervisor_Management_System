3abd4d993b13e915e3eef5c38653d24181a2193e
Anushree Vinod <anushreevinod10@gmail.com>
2025-03-15 21:35:05 +0000

    add code to create superuser in build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,19 @@ python backend/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python backend/manage.py migrate
+
+# Check if the superuser exists, and create one if it doesn't
+python backend/manage.py shell <<EOF
+from user_management.models import BaseUser
+from django.core.management import call_command
+
+# Check if a superuser exists
+if not BaseUser.objects.filter(is_superuser=True).exists():
+    print("Superuser does not exist. Creating one...")
+    # Create superuser without password
+    user = BaseUser.objects.create_superuser('admin@example.com', 'adminpassword123')  # Create the user and set the password
+    print("Superuser created.")
+
+else:
+    print("Superuser already exists.")
+EOF
--- backend/user_management/managers.py
+++ backend/user_management/managers.py
@@ -12,3 +12,16 @@ class CustomBaseUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email, username, and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        # Ensure the password is set, even if it is empty
+        if password is None:
+            raise ValueError('Superusers must have a password')
+
+        return self.create_user(email, password, **extra_fields)
