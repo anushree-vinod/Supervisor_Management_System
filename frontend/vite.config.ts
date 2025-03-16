50f009c2eacf7644e8392fa04978a054f85720a5
Anushree Vinod <anushreevinod10@gmail.com>
2025-03-16 00:53:30 +0000

    configure base url for prod and dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as dotenv from 'dotenv';
 
-// Load environment variables from .env file
-dotenv.config();
+export default ({ mode }: { mode: string }) => {
+  // Load environment variables based on the current mode (development/production)
+  const env = loadEnv(mode, process.cwd());
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.DEBUG === 'false' || !process.env.DEBUG ? '/Supervisor_Management_System/' : '/',
-})
+  // You can directly use `process.env` after loading the environment variables
+  process.env = { ...process.env, ...env };
+
+  return defineConfig({
+    plugins: [react()],
+    // Use the loaded environment variables for base path
+    base: process.env.VITE_DEBUG === 'false' ? '/Supervisor_Management_System/' : '/',
+  });
+};
